3b669cdff39ee45fa23648128150216b4e718c79
tiagotml <45052602+tiagotml@users.noreply.github.com>
2022-11-22 11:20:53 -0300

    fist

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import poweredImage from './assets/powered.png';
+import styles from './App.module.css';
+import { useState } from 'react';
+const App = () => {
+  const [heightField, setHeightField] = useState<number>(0);
+  const [weightField, setWeightField] = useState<number>(0);
 
-function App() {
+  const handleCalculateButton = () => {
+    if (heightField && weightField) {
+
+    }
+    else{
+      alert("Digite todos os campos")
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={poweredImage} alt="" width={150}></img>
+        </div>
       </header>
-    </div>
-  );
-}
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          <h1>Calcule o seu IMC.</h1>
+          <p>IMC é a sigla para Índice de Massa Corpórea, parâmentro adotado pela Organização Mundial de Saúde para calcular o peso ideal de cada pessoa.</p>
 
-export default App;
+          <input
+            type="number"
+            placeholder="Digite a sua altura. Ex 1.5(em métros)"
+            value={heightField > 0 ? heightField : ''}
+            onChange={e => setHeightField(parseFloat(e.target.value))}
+
+          />
+
+          <input
+            type="number"
+            placeholder="Digite o seu peso. Ex 75.3(em kg)"
+            value={weightField > 0 ? weightField : ''}
+            onChange={e => setWeightField(parseFloat(e.target.value))}
+
+          />
+
+          <button onClick={handleCalculateButton}>Calcular</button>
+        </div>
+        <div className={styles.rightSide}>
+          ...
+        </div>
+      </div>
+    </div>
+  )
+}
+export default App
